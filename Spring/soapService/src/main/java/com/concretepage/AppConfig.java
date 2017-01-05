d04400c29afaa8c5cc240800bae0d6ee4d4f28e7
parkito <artyom-karnov@yandex.ru>
2017-01-06 00:18:39 +0300

    fixed poms

--- Spring/soapService/src/main/java/com/concretepage/AppConfig.java
+++ Spring/soapService/src/main/java/com/concretepage/AppConfig.java
@@ -12,21 +12,21 @@ import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
-@ComponentScan("com.concretepage") 
+@ComponentScan("com.concretepage")
 public class AppConfig extends WsConfigurerAdapter {
 
-	@Bean(name = "users")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentsSchema) {
-		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-		wsdl11Definition.setPortTypeName("UsersPort");
-		wsdl11Definition.setLocationUri("/soapws");
-		wsdl11Definition.setTargetNamespace("http://localhost:8080");
-		wsdl11Definition.setSchema(studentsSchema);
-		return wsdl11Definition;
-	}
+    @Bean(name = "users")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentsSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("UsersPort");
+        wsdl11Definition.setLocationUri("/soapws");
+        wsdl11Definition.setTargetNamespace("http://localhost:8080");
+        wsdl11Definition.setSchema(studentsSchema);
+        return wsdl11Definition;
+    }
 
-	@Bean
-	public XsdSchema studentsSchema() {
-		return new SimpleXsdSchema(new ClassPathResource("users.xsd"));
-	}
+    @Bean
+    public XsdSchema studentsSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("users.xsd"));
+    }
 }
