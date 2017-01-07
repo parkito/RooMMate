f1545ef7907905092cf65f0fe46b6a3360a06d1f
parkito <artyom-karnov@yandex.ru>
2017-01-08 02:42:23 +0300

    spring boot transformation#3

---
+++ Spring/webResolver/src/main/java/resolvers/ServletInitializer.java
@@ -0,0 +1,25 @@
+package resolvers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author Artem Karnov @date 08.01.17.
+ *         artem.karnov@t-systems.com
+ **/
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.portal"})
+@EntityScan("com.portal")
+@EnableJpaRepositories("com.portal")
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
+
