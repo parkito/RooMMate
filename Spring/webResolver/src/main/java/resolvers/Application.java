d91949a40de13c1c31c0dc4ea8683aa8ff497b5b
parkito <artyom-karnov@yandex.ru>
2017-01-08 02:01:40 +0300

    spring boot transformation#1

---
+++ Spring/webResolver/src/main/java/resolvers/Application.java
@@ -0,0 +1,20 @@
+package resolvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+/**
+ * @author Artem Karnov @date 07.01.17.
+ *         artem.karnov@t-systems.com
+ **/
+@Configuration
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableCaching
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ Spring/webResolver/src/tests/java/resolvers/webPagesResolver/controllers/WebControllerTest.java
@@ -0,0 +1,73 @@
+package resolvers.webPagesResolver.controllers;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Artem Karnov @date 08.01.17.
+ *         artem.karnov@t-systems.com
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {TestContext.class, WebAppContext.class})
+public class WebControllerTest extends TestCase {
+    @Before
+    public void cleaning() {
+    }
+
+    public void testInitialization() throws Exception {
+
+    }
+
+    public void testDeleteAll() throws Exception {
+
+    }
+
+    public void testAddUser() throws Exception {
+
+    }
+
+    public void testAddGroup() throws Exception {
+
+    }
+
+    public void testAddRoom() throws Exception {
+
+    }
+
+    public void testGetUser() throws Exception {
+
+    }
+
+    public void testGetGroup() throws Exception {
+
+    }
+
+    public void testGetRoom() throws Exception {
+
+    }
+
+    public void testAddUserToGroup() throws Exception {
+
+    }
+
+    public void testAddGroupToRoom() throws Exception {
+
+    }
+
+    public void testDeleteUser() throws Exception {
+
+    }
+
+    public void testDeleteGroup() throws Exception {
+
+    }
+
+    public void testDeleteRoom() throws Exception {
+
+    }
+
+}
