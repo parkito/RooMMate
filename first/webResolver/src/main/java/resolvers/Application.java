5137d7a3a2d8cf9a3a5106fba0ccaa50650e8e0a
parkito <artyom-karnov@yandex.ru>
2017-01-21 01:48:01 +0300

    web path found

---
+++ first/webResolver/src/main/java/resolvers/Application.java
@@ -0,0 +1,30 @@
+package resolvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author Artem Karnov @date 07.01.17.
+ *         artem.karnov@t-systems.com
+ **/
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.portal, resolvers, com.concretepage"})
+@EntityScan("com.portal")
+@EnableJpaRepositories("com.portal")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ first/webResolver/src/main/java/resolvers/restResolver/controllers/RestController.java
@@ -0,0 +1,33 @@
+package resolvers.restResolver.controllers;
+
+//import com.portal.dto.UserDTO;
+//import com.portal.entities.User;
+//import com.portal.services.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Artem Karnov @date 07.01.17.
+ *         artem.karnov@t-systems.com
+ **/
+@Controller("RestController")
+public class RestController {
+//    @Autowired
+//    UserService userService;
+
+//    @RequestMapping(value = "/rest/getUser", method = RequestMethod.GET)
+//    public
+//    @ResponseBody
+//    UserDTO getUser(@RequestParam(value = "email") String email) {
+////        UserDTO userDTO = new UserDTO();
+////        User user = userService.getUserByEMAil(email);
+////        userDTO.setName(user.getName());
+////        userDTO.setSecondName(user.getSecondName());
+////        userDTO.setEmail(user.getEmail());
+//        return userDTO;
+//    }
+}
