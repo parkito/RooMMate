2d8ad8c8b0397806325bffe0df2fab8ba964f521
parkito <artyom-karnov@yandex.ru>
2017-02-13 22:16:53 +0300

    work with rest-client #1

--- MicroServices/GUIChat/src/main/java/com/portal/controller/WelcomeController.java
+++ MicroServices/GUIChat/src/main/java/com/portal/controller/WelcomeController.java
@@ -1,7 +1,10 @@
 package com.portal.controller;
 
 import com.portal.entity.User;
+import com.portal.exceptions.DAOException;
+import com.portal.exceptions.ResponseEntityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +20,13 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class WelcomeController {
     private final static String REST_ADDRESS_OF_PI_SERVER = "http://localhost:8083/rest/";
+    private final static String GET_USER = "getUser?";
+    private final static String AND = "&";
+    private final static String EMAIL_E = "eMail=";
+    private final static String NAME_E = "name=";
+    private final static String SECOND_NAME_E = "secondName=";
+    private final static String PASSWORD_E = "password=";
+    private final static String ADD_USER = "addUser?";
 
     @Autowired
     RestTemplate restTemplate;
@@ -24,12 +34,6 @@ public class WelcomeController {
     @RequestMapping("/")
     public String welcome() {
         return "welcome";
-    }
-
-    public User getUser(String eMail) {
-        User user = restTemplate.getForObject(REST_ADDRESS_OF_PI_SERVER + "getUser?eMail="
-                + eMail, User.class);
-        return user;
     }
 
     @RequestMapping(value = "/getUser", method = RequestMethod.GET)
@@ -42,14 +46,75 @@ public class WelcomeController {
         return "welcome";
     }
 
-    public String addUser(String name, String secondName, String eMail, String password) {
-        User user = restTemplate.getForObject(REST_ADDRESS_OF_PI_SERVER + "addUser?name="
-                + name + "&" + "secondName="
-                + secondName + "&" + "eMail="
-                + eMail + "&" + "password="
-                + password, User.class);
-        return "hello";
+    public void addUser(String name, String secondName, String eMail, String password) {
+        ResponseEntity response;
+        try {
+            response = restTemplate.getForObject(REST_ADDRESS_OF_PI_SERVER + ADD_USER +
+                            NAME_E + name + AND +
+                            SECOND_NAME_E + secondName + AND +
+                            EMAIL_E + eMail + AND +
+                            PASSWORD_E + password,
+                    ResponseEntity.class);
+        } catch (Exception ex) {
+            throw new ResponseEntityException("Error in addUsers while adding " + eMail, ex);
+        }
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new DAOException("User " + eMail + " wasn't added because " + response.getStatusCode());
+        }
     }
+
+    public User getUser(String eMail) {
+        User user = null;
+        try {
+            user = restTemplate.getForObject(
+                    REST_ADDRESS_OF_PI_SERVER + GET_USER + EMAIL_E
+                            + eMail, User.class);
+        } catch (Exception ex) {
+            throw new ResponseEntityException("Error in getUsers while getting " + eMail, ex);
+        }
+        return user;
+    }
+
+
+
+    /*
+    public List<RodsMalfunctionDataDiagnosticEvent> getEvents(Date from, Date to, String driverUuid) {
+        log.debug("getMalfunctionAndDiagnosticEventsBetween(driverUuid={}, from={}, to={})" + driverUuid + from + to);
+        String baseUri = Configuration.getString(PortalExtProperties.REST_RODS_SERVICE_ENDPOINT_URL);
+
+        URI targetUrl = UriComponentsBuilder.fromUriString(baseUri).pathSegment("malfunction-data-diagnostic-event")
+                .queryParam("rsqlQuery", rsqlQueryBuilder.findByEventTimestampBetween(driverUuid, from, to))
+                .queryParam("sort", "eventTimestamp,ASC") //see spring data web support
+                .queryParam("page", 0)
+                .queryParam("size", Integer.MAX_VALUE).build().toUri();
+
+        ResponseEntity<Page<RodsMalfunctionDataDiagnosticEvent>> responseEntity = restTemplate.exchange(
+                targetUrl, HttpMethod.GET, HttpEntity.EMPTY,
+                new ParameterizedTypeReference<Page<RodsMalfunctionDataDiagnosticEvent>>() {
+                });
+        List<RodsMalfunctionDataDiagnosticEvent> result = responseEntity.getBody().getContent();
+        log.debug("getMalfunctionAndDiagnosticEventsBetween: result = {}" + JsonPrinter.print(result));
+        return result;
+    }
+
+
+@RequestMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public ResponseEntity save(@RequestBody RodsData rodsData) {
+        try {
+            rodsService.save(rodsData);
+        } catch (Exception e) {
+            logger.error("Save error {}", e);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+
+
+
+    */
 
 }
 
