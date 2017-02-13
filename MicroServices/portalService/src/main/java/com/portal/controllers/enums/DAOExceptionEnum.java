0b4ef64ea35b0c90255d68df54a5d094eee43119
parkito <artyom-karnov@yandex.ru>
2017-02-13 22:43:07 +0300

    work with rest-client #1

---
+++ MicroServices/portalService/src/main/java/com/portal/controllers/enums/DAOExceptionEnum.java
@@ -0,0 +1,13 @@
+package com.portal.controllers.enums;
+
+/**
+ * @author Artem Karnov @date 13.02.2017.
+ *         artem.karnov@t-systems.com
+ */
+
+public enum  DAOExceptionEnum {
+    USER_NOT_FOUND_ENUM,
+    ROOM_NOT_FOUND,
+    GROUP_NOT_FOUND,
+    // TODO: 13.02.2017 finish it
+}
