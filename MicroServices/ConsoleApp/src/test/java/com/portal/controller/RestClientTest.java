e4b56ba14e35b945302b3bb3f9128b20a01027b1
parkito <artyom-karnov@yandex.ru>
2017-03-16 01:21:47 +0300

    Rest unit testing #1

--- MicroServices/ConsoleApp/src/test/java/com/portal/controller/RestClientTest.java
+++ MicroServices/ConsoleApp/src/test/java/com/portal/controller/RestClientTest.java
@@ -2,7 +2,10 @@ package com.portal.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Artem Karnov @date 16.03.17.
@@ -10,10 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = {RestClient.class,RestTemplate.class})
 public class RestClientTest {
+
+    @Autowired
+    RestClient restClient;
 
     @Test
     public void firstTest() {
+        restClient.getUser("email1", "password1");
     }
 
 }
