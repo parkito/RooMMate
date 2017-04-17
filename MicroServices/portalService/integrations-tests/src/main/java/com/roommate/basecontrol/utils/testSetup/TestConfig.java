19b8026a2c73e9c72cfbac9e21854cda08841a85
parkito <artyom-karnov@yandex.ru>
2017-04-17 20:38:32 +0300

    added liquid base change sets

--- MicroServices/portalService/integrations-tests/src/main/java/com/roommate/basecontrol/utils/testSetup/TestConfig.java
+++ MicroServices/portalService/integrations-tests/src/main/java/com/roommate/basecontrol/utils/testSetup/TestConfig.java
@@ -5,10 +5,19 @@ package com.roommate.basecontrol.utils.testSetup;
  * artem.karnov@t-systems.com
  **/
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.roommate.basecontrol")
 public class TestConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
 }
