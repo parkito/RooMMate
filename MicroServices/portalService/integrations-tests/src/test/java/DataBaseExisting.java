95cd9ae87619cd96e8b7ae09fb1c8f83560426fe
parkito <artyom-karnov@yandex.ru>
2017-04-19 21:48:01 +0300

    added test module

---
+++ MicroServices/portalService/integrations-tests/src/test/java/DataBaseExisting.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Artem Karnov @date 19.04.2017.
+ *         artem.karnov@t-systems.com
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class})
+public class DataBaseExisting {
+}
